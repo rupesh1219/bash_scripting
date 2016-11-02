118b6d235c1a61408f812f119365957c7d6e15b2
rupesh1219 <rupesh1219@gmail.com>
2016-11-02 14:28:40 -0400

    rerunning logic when sequential jobs failed

--- jobschedule_test.sh
+++ jobschedule_test.sh
@@ -4,96 +4,71 @@ interpreter="/home/rupeshkepler/Documents/rupesh_github/bash_scripting//venv/bin
 
 path="/home/rupeshkepler/Documents/rupesh_github/bash_scripting/testing_bash/"
 
-rm $path/status.txt
+check_job1=$( grep "success*" $path/status1.txt )
+check_job2=$( grep "success*" $path/status2.txt )
+check_job3=$( grep "success*" $path/status3.txt )
 
-($interpreter $path/problem1.py > $path/problem1.log;
-RESULT1=$?
-echo $RESULT1'job1'
+echo $check_job1
+echo $check_job2
+echo $check_job3
 
-if [ $RESULT1 -eq 0 ];
+(if [[ "$check_job1" != success* ]];
 then
-    echo 'success1' >> $path/status.txt
+    ($interpreter $path/problem1.py > $path/problem1.log;
+     RESULT1=$?
+     echo $RESULT1'job1'
+
+     if [ $RESULT1 -eq 0 ];
+     then
+         echo 'success1' $(date) > $path/status1.txt
+     else
+         echo 'error1' $(date) > $path/status1.txt
+     fi
+    )
 else
-    echo 'error1' >> $path/status.txt
-fi
-) &
+    echo 'job1 ran before successfully' $(date)
+ fi)&
+(if [[ "$check_job2" != success* ]];
+then
+    ($interpreter $path/problem2.py > $path/problem2.log;
+     RESULT2=$?
+     echo $RESULT2'job2'
 
-($interpreter $path/problem2.py > $path/problem2.log;
- RESULT2=$?
- echo $RESULT2'job2'
-
- if [ $RESULT2 -eq 0 ];
- then
-     echo 'success2' >> $path/status.txt
- else
-     echo 'error2' >> $path/status.txt
- fi
-)
+     if [ $RESULT2 -eq 0 ];
+     then
+         echo 'success2' $(date) > $path/status2.txt
+     else
+         echo 'error2' $(date) > $path/status2.txt
+     fi
+    )
+else
+    echo 'job2 ran before successfully' $(date)
+fi)
 
 wait
 
-check1=$( grep "error*" $path/status.txt )
+check1=$( grep "error*" $path/status1.txt )
+check2=$( grep "error*" $path/status2.txt )
 echo $check1
+echo $chekc2
 
-if [[ "$check1" =~ error* ]];
+if [[ "$check1" =~ error* ]] || [[ "$check2" =~ error* ]];
 then
-    echo 'leve1 jobs faild' >> $path/status.txt
-else
+    echo 'leve1 jobs failed and we cant run this job' $(date) > $path/status3.txt
+elif [[ "$check_job3" != success* ]];
+then
     $interpreter $path/problem3.py > $path/problem3.log;
     RESULT4=$?
 
     if [ $RESULT4 -eq 0 ];
     then
-        echo 'success3' >> $path/status.txt
+        echo 'success3' $(date) > $path/status3.txt
     else
-        echo 'error3' >> $apth/status.txt
+        echo 'error3' $(date) > $path/status3.txt
     fi
-fi
-
-
-echo 'workflow completed'
-
-
-
-
-
-
-
-
-:'
-if [ $? == 0 ];
-then
-    output1=0
 else
-    output1=1
+    echo 'job 3 ran successfully before after running 1&2' $(date)
 fi
-) &
-
-($interpreter $path/problem2.py > $path/problem2.log;
- if [ $? == 0 ];
- then
-    output2=0
- else
-    output2=1
- fi
-);
-
 wait
-echo $output1
-echo $output2
 
-if
-    [ $output1==0 ] && [ $output2==0 ] ;
-then
-    $interpreter $path/problem3.py > $path/problem3.log;
-    if [ $? == 0 ];
-       then
-           output3=0
-       else
-           output3=1
-    fi
-fi
-
-wait
-echo $output3
-'
+echo 'workflow completed' $(date)
