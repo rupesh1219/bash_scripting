a1bdc9fa5c87a9689750c31b711f0e36b8935184
rupesh1219 <rupesh1219@gmail.com>
2016-11-04 14:45:10 -0400

    added more jobs to the schedule

--- jobschedule_test.sh
+++ jobschedule_test.sh
@@ -7,10 +7,14 @@ path="/home/rupeshkepler/Documents/rupesh_github/bash_scripting/testing_bash/"
 check_job1=$( grep "success*" $path/status1.txt )
 check_job2=$( grep "success*" $path/status2.txt )
 check_job3=$( grep "success*" $path/status3.txt )
+check_job4=$( grep "success*" $path/status4.txt )
+check_job5=$( grep "success*" $path/status5.txt )
 
 echo $check_job1
 echo $check_job2
 echo $check_job3
+echo $check_job4
+echo $check_job5
 
 (if [[ "$check_job1" != success* ]];
 then
@@ -69,6 +73,52 @@ then
 else
     echo 'job 3 ran successfully before after running 1&2' $(date)
 fi
+wait
+
+check3=$( grep "error*" $path/status3.txt )
+echo $check3
+
+if [[ "$check3" =~ error* ]];
+then
+    echo 'level 2 jobs failed and we  cant run this job' $(date)
+elif [[ "$check_job4" != "success*" ]];
+then
+    $interpreter $path/problem4.py > $path/problem4.log;
+    RESULT5=$?
+
+    if [ $RESULT5 -eq 0 ];
+    then
+        echo 'success4' $(date) > $path/status4.txt
+    else
+        echo 'error4' $(date) > $path/status4.txt
+    fi
+else
+    echo 'job 4 ran successfully after running level 2 jobs' $(date)
+fi
+
+check4=$( grep "error*" $path/status4.txt )
+echo $check4
+
+#wait
+
+if [[ "$check4" =~ error* ]];
+then
+    echo 'level 3 job failed  and we cant run this job' $(date) > $path/status5.txt
+elif [[ "$check_job5" != "success*" ]];
+then
+    $interpreter $path/problem5.py > $path/problem5.log;
+    RESULT6=$?
+
+    if [ $RESULT6 -eq 0 ];
+    then
+        echo 'success5' $(date) > $path/status5.txt
+    else
+        echo 'error5' $(date) > $path/status5.txt
+    fi
+else
+    echo 'job 5 ran successfully after completing level 3 jobs' $(date)
+fi
+
 wait
 
 echo 'workflow completed' $(date)
